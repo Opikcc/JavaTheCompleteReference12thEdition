b5bdc04b0fa5da9480bf9f7bb68ab85d08bbdf79
Babaliong <MTaufik@Outlook.Com>
2022-04-20 01:18:41 +0700

    Control Statements

---
+++ src/ControlStatements/DoWhile.java
@@ -0,0 +1,14 @@
+package ControlStatements;
+
+// Demonstrate the do-while loop.
+class DoWhile {
+
+  public static void main(String[] args) {
+    int n = 10;
+
+    do {
+      System.out.println("tick " + n);
+      n--;
+    } while (n > 0);
+  }
+}
